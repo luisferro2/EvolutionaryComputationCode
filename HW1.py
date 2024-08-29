9bc1774db13491365bca185788bdbdc6d77d920f
luisferro2 <a01378248@itesm.mx>
2024-08-29 17:55:23 -0600

    student ids added

--- HW1.py
+++ HW1.py
@@ -2,7 +2,7 @@
 Code for homework #1 Evolutionary Computing Class
 Authors:
 Luis Ignacio Ferro Salinas A01378248
-Iván Miguel García López
+Iván Miguel García López A01686450
 """
 
 import numpy as np
